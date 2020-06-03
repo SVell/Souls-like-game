4016cd273ee545c43c030fbccfbbe0d0412d7fa5
Viacheslav <surzhenkoviacheslav@gmail.com>
2020-06-03 18:53:26 +0300

    Jump
    Gravity and Jump

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -11,8 +11,17 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 4;
     public float turnSmoothTime = 0.1f;
     private float turnSmoothVelocity;
+    private Vector3 velocity;
 
     public Animator anim;
+    
+    // Jump
+    public float gravity = -9.81f;
+    public float jumpForce = 3f;
+    public Transform groundCheck;
+    public float groundDistance;
+    public LayerMask whatIsGrounded;
+    private bool isGrounded;
 
     private void Start()
     {
@@ -22,7 +31,7 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Jump();
         Walk();
     }
 
@@ -63,6 +72,25 @@ public class PlayerMovement : MonoBehaviour
         {
             anim.SetInteger("MoveStates",0);
         }
+    }
+
+    void Jump()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, whatIsGrounded);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+            anim.SetBool("Jump",false);
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            anim.SetBool("Jump",true);
+            velocity.y = (float)Math.Sqrt(jumpForce * -2f * gravity);
+        }
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
     
 }
